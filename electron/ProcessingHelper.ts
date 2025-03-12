b6cffe0ddd75af5dc9b77c1d1f4603a7ade9da87
Logesh M <mlogesh2996@gmail.com>
2025-03-13 03:14:19 +0530

    Backdrop not coming in production issue fix

--- electron/ProcessingHelper.ts
+++ electron/ProcessingHelper.ts
@@ -9,7 +9,7 @@ import { BrowserWindow } from "electron"
 const isDev = !app.isPackaged
 const API_BASE_URL = isDev
   ? "http://localhost:3000"
-  : "https://www.interviewcoder.co"
+  : "https://crack-coding-interview.vercel.app/"
 
 export class ProcessingHelper {
   private deps: IProcessingHelperDeps
